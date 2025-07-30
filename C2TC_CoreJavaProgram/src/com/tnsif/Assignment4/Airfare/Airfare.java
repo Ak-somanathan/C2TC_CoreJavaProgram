b677d5b7388affdfe1a5f6bcbfdf34aa6f1b85e4
LENOVO <LENOVO@DESKTOP-E8HS1G8>
2025-07-30 15:07:41 +0530

    java programs

---
+++ C2TC_CoreJavaProgram/src/com/tnsif/Assignment4/Airfare/Airfare.java
@@ -0,0 +1,5 @@
+package com.tnsif.Assignment4.Airfare;
+
+public interface Airfare {
+    double calculateAmount();
+}
---
+++ C2TC_CoreJavaProgram/src/com/tnsif/Assignment4/Dmart.java
@@ -0,0 +1,5 @@
+package com.tnsif.Assignment4;
+
+public interface Dmart {
+    void displayItems();
+}
